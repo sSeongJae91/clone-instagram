25abdaed748a8eaef5f2dc3a3d4c4db01c97ced4
sSeongjae91 <ben91429@gmail.com>
2021-05-03 23:26:57 +0900

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,86 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import { View, Text } from 'react-native';
+import firebase from 'firebase/app';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import LandingScreen from './components/auth/Landing';
+import RegisterScreen from './components/auth/Register';
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAvi01V2iFunN2DfvA2mZk5GpKpIqaFFcM",
+  authDomain: "instagram-dev-3adea.firebaseapp.com",
+  projectId: "instagram-dev-3adea",
+  storageBucket: "instagram-dev-3adea.appspot.com",
+  messagingSenderId: "984647833608",
+  appId: "1:984647833608:web:cc7ad455f85fdd1af799bd",
+  measurementId: "G-8HV990HJYN"
+};
+
+const Stack = createStackNavigator();
+
+
+export class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loaded: false,
+    }
+  }
+
+  componentDidMount() {
+    firebase.initializeApp(firebaseConfig);
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if(!user) {
+        this.setState({
+          loggedIn: false,
+          loaded: true
+        })
+      }else {
+        this.setState({
+          loggedIn: true,
+          loaded: true
+        })
+      }
+    })
+  }
+
+  render() {
+
+    const { loggedIn, loaded } = this.state;
+
+    if(!loaded) {
+      return (
+        <View style={{flex: 1, justifyContent: 'center'}}>
+          <Text>Loading</Text>
+        </View>
+      )
+    }
+
+    if(!loggedIn) {
+      return (
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName="Landing">
+            <Stack.Screen name="Landing" component={LandingScreen} options={{ headerShown: false }}/>
+            <Stack.Screen name="Register" component={RegisterScreen}/>
+          </Stack.Navigator>
+        </NavigationContainer>
+      )
+    }
+
+    return (
+      <View style={{flex: 1, justifyContent: 'center'}}>
+        <Text>User is logged in</Text>
+      </View>
+    )
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
---
+++ README.md
@@ -0,0 +1,33 @@
+## settings
+
+### expo install
+- $ npm install --global expo-cli
+- $ expo init instagram
+
+### expo run
+- $ expo start
+
+
+### libraries
+
+#### react native navigation === router
+- $ npm install @react-navigation/native
+
+#### @react-navigation/stack
+ - $npm install @react-navigation/stack
+
+#### react-native-screens
+- $ npm i react-native-screens
+
+#### react-native-gesture-handler
+- $ npm i react-native-gesture-handler
+
+#### @react-native-community/masked-view
+- $ npm i @react-native-community/masked-view
+
+#### firebase
+ - $ npm install --save firebase
+
+
+### extensions
+- ES7 React/Redux/GraphQL/React-Native snippets
