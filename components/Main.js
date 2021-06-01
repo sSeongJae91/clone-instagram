ec992caf087fb5fb654a42778f511de82aa28bf3
sSeongjae91 <ben91429@gmail.com>
2021-06-01 23:30:52 +0900

    profile

--- components/Main.js
+++ components/Main.js
@@ -3,6 +3,8 @@ import React, { Component } from 'react';
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
+import firebase from 'firebase';
+
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { fetchUser, fetchUserPosts } from '../redux/actions/index';
@@ -53,6 +55,12 @@ export class Main extends Component {
                         ),
                     }}/>
                 <Tab.Screen name="Profile" component={ProfileScreend} 
+                    listeners={({ navigation }) => ({
+                        tabPress: event => {
+                            event.preventDefault();
+                            navigation.navigate("Profile", {uid: firebase.auth().currentUser.uid});
+                        }
+                    })}
                     options={{
                         tabBarIcon: ({ color, size }) => (
                             <MaterialCommunityIcons name="account-circle" color={color} size={26} />
--- components/main/Profile.js
+++ components/main/Profile.js
@@ -1,25 +1,68 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, View, Text, Image, FlatList } from 'react-native';
+
+import firebase from 'firebase';
+require('firebase/firestore')
 
 import { connect } from 'react-redux';
 
 function Profile(props) {
-    const { currentUser, posts } = props;
+    const [userPosts, setUserPosts] = useState([]);
+    const [user, setUser] = useState(null);
 
-    console.log({currentUser, posts});
+    useEffect(() => {
+        const { currentUser, posts } = props;
+        console.log({currentUser, posts});
+
+        if(props.route.params.uid === firebase.auth().currentUser.uid) {
+            setUser(currentUser);
+            setUserPosts(posts);
+        }else {
+            firebase.firestore()
+                .collection("users")
+                .doc(props.route.params.uid)
+                .get()
+                .then((snapshot) => {
+                    if(snapshot.exists) {
+                        setUser(snapshot.data());
+                    }else {
+                        console.log('does not exist')
+                    }
+                })
+            firebase.firestore()
+                .collection("posts")
+                .doc(props.route.params.uid)
+                .collection("userPosts")
+                .orderBy("creation", "asc")
+                .get()
+                .then((snapshot) => {
+                    let posts = snapshot.docs.map(doc => {
+                        const data = doc.data();
+                        const id = doc.id;
+    
+                        return { id, ...data }
+                    })
+                    setUserPosts(posts);
+                })
+        }
+    }, [props.route.params.uid])
+    
+    if(user === null) {
+        return <View/>
+    }
 
     return (
         <View Styles={styles.container}>
             <View style={styles.containerInfo}>
-                <Text>{currentUser.name}</Text>
-                <Text>{currentUser.email}</Text>
+                <Text>{user.name}</Text>
+                <Text>{user.email}</Text>
             </View>
             
             <View style={styles.containerGallery}>
                 <FlatList
                     numColumns={3}
                     horizontal={false}
-                    data={posts}
+                    data={userPosts}
                     renderItem={({item}) => (
                         <View style={styles.containerImage}>
                             <Image
@@ -36,8 +79,7 @@ function Profile(props) {
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        marginTop: 40
+        flex: 1
     },
     containerInfo: {
         margin: 10
